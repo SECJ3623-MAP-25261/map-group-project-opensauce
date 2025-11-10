8218b87dd3222896e9f4e26e04ee0b23400e9f8e
lee hom <102334679+leehom22@users.noreply.github.com>
2025-11-10 14:45:06 +0800

    creating constants folder

---
+++ lib/data/constants/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFF8BE17); // Gold color
+  
+}
+
+class KTextStyle {
+  static const TextStyle resetPasswordTitle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 30.0,
+  );
+
+  static const TextStyle resetPasswordDescription = TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 15.0,
+  );
+}
