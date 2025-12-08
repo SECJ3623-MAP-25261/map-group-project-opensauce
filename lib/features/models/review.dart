4f4e15949b4ae8b568e6f74070969da7673ddbc8
Soo Zhen Yang <blueiceyang55@gmail.com>
2025-12-08 08:33:45 +0800

    changes on review models

--- lib/features/models/review.dart
+++ lib/features/models/review.dart
@@ -1,23 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Review {
-  final double star; // Rating out of 5
-  final String reviewText; // The written review
+  final String reviewerName;
+  final String reviewerImage;
+  final DateTime date;
+  final double star;
+  final String reviewText;
 
   Review({
+    required this.reviewerName,
+    required this.reviewerImage,
+    required this.date,
     required this.star,
     required this.reviewText,
   });
 
-  factory Review.fromMap(Map<String, dynamic> map) {
-    return Review(
-      star: (map['star'] as num?)?.toDouble() ?? 0.0,
-      reviewText: map['reviewText'] ?? '',
-    );
-  }
-
   Map<String, dynamic> toMap() {
     return {
+      'reviewerName': reviewerName,
+      'reviewerImage': reviewerImage,
+      'date': Timestamp.fromDate(date),
       'star': star,
       'reviewText': reviewText,
     };
+  }
+
+  factory Review.fromMap(Map<String, dynamic> map) {
+    return Review(
+      reviewerName: map['reviewerName'] ?? 'Anonymous',
+      reviewerImage: map['reviewerImage'] ?? 'https://i.pravatar.cc/150?img=1',
+      date: (map['date'] as Timestamp).toDate(),
+      star: (map['star'] as num?)?.toDouble() ?? 0.0,
+      reviewText: map['reviewText'] ?? '',
+    );
   }
 }
