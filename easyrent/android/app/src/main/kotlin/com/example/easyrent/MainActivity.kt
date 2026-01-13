b65afe9a4b9bfc32b9c7feabc87626c0f217d673
lee hom <102334679+leehom22@users.noreply.github.com>
2026-01-13 20:21:30 +0800

    temporarily remove new ver easyRent

--- easyrent/android/app/src/main/kotlin/com/example/easyrent/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.easyrent
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- easyrent/functions/src/routes/dashboard_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const dashboardController = require("../controllers/dashboard_controller");
-
-const router = express.Router();
-
-// GET /renter-stats?userId=...
-router.get("/renter-stats", dashboardController.getRenterStats);
-
-module.exports = router;
